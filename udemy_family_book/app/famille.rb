81fc3774d4ab56629a37a5b10d3c8d68825683f1
R3m1 <r3m1.informatique@gmail.com>
2018-07-25 09:30:03 +0200

    Version d'origine de l'exercice

---
+++ udemy_family_book/app/famille.rb
@@ -0,0 +1,27 @@
+require "json"
+
+class Famille
+    attr_accessor :nom, :age, :titre, :mail, :telephone
+
+    def initialize (*args)
+        @nom = args[0]
+        @age = args[1]
+        @titre = args[2]
+        @mail = args[3]
+        @telephone = args[4]
+    end
+
+    def sauvegarde
+        membre_input = {
+            nom: @nom,
+            age: @age,
+            titre: @titre,
+            mail: @mail,
+            telephone: @telephone
+        }.to_json
+
+        open("livret.json", "a") do |fichier|
+            fichier.puts membre_input + ","
+        end
+    end
+end
---
+++ udemy_family_book/run.rb
@@ -0,0 +1,30 @@
+require_relative "./app/famille"
+
+puts "Désirez-vous ajouter un nouveau membre au livret de famille ? (y/n)"
+answer = gets.chomp.upcase
+
+while answer == "Y"
+    puts "Veuillez saisir le nom du membre a ajouter :"
+    nom = gets.chomp.capitalize
+    
+    puts "Veuillez saisir l'age du membre a ajouter :"
+    age = gets.chomp.to_i
+    
+    puts "Veuillez saisir le titre du membre a ajouter :"
+    titre = gets.chomp
+
+    puts "Veuillez saisir le mail du membre a ajouter :"
+    mail = gets.chomp
+
+    puts "Veuillez saisir le téléphone du membre a ajouter :"
+    telephone = gets.chomp
+
+    nouveau_membre = Famille.new(nom, age, titre, mail, telephone)
+    nouveau_membre.sauvegarde
+
+    puts "Désirez-vous ajouter un autre membre ? (y/n)"
+    again = gets.chomp.upcase
+    if again != "Y"
+        break
+    end
+end
