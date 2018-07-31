051462a90e7b64abb3e6092fbaa3138dcfdd8eb7
R3m1 <r3m1.informatique@gmail.com>
2018-08-01 00:55:17 +0200

    Code apres chap. 7 : mise en place fonctions

--- taskman/taskman.rb
+++ taskman/taskman.rb
@@ -6,59 +6,69 @@
 #Utilisation de p plutôt que puts pour conserver la forme de tableau à l'affichage
 #p ARGV
 
-#Récupération du premier argument et mise en minuscule
-command = ARGV.shift.downcase
-
 #Initialisation du tableau des taches
-tableau_taches = [
+$tableau_taches = [
     {id: 0, :content => "Ameliorer taskman", flags: %w(important urgent)}
     #[0, "Ameliorer taskman", "flags", "important urgent"]
 ]
 
-case command
-when "add"
-    commande = ARGV.shift
+OPTIONS_DEFAULT = {
+    flags: [],
+    date: nil
+}
+
+def parser_command command
+    case command
+    when "add"
+        $tableau_taches << ajouter(ARGV)
+    when "mod"
+        puts "Commande MOD"
+    when "del"
+        supprimer(ARGV.shift.to_i)
+    else
+        puts "Commande non reconnue"
+    end
+end
+
+def afficher
+    puts "*****TASKMAN*****"
+    puts "LISTE DES TACHES"
+
+    $tableau_taches.each do |tache|
+        puts "#{tache[:id]} - #{tache[:content]} (#{tache[:flags].join(" ")})"
+    end
+end
+
+def supprimer id
+    $tableau_taches = $tableau_taches.reject{|tache| tache[:id] == id.to_i}
+end
+
+def ajouter params
+    commande = params.shift
 
     tache = {}
 
-    tache[:id] = tableau_taches.map{ |tache| tache[:id] }.max + 1
+    tache[:id] = $tableau_taches.map{ |tache| tache[:id] }.max + 1
 
-    OPTIONS_DEFAULT = {
-        flags: [],
-        date: nil
-    }
+    
     tache.merge!(OPTIONS_DEFAULT)
 
     tache[:content] = commande
 
-    ARGV.each do |argument|
+    params.each do |argument|
         champ, valeur = argument.split(':')
-
-        tache[champ.to_sym] = valeur
-
-        #case champ
-        #when "flags"
-        #    puts "Ajouter Flags : #{valeur}"
-        #when "date"
-        #    puts "Ajouter Date"
-        #else
-        #    puts "Je ne comprend pas : #{argument}"
-        #end
+        if champ == "flags"
+            tache[champ.to_sym] = valeur.split(",")
+        else
+            tache[champ.to_sym] = valeur
+        end
     end
 
-    tableau_taches << tache
-when "mod"
-    puts "Commande MOD"
-when "del"
-    id = ARGV.shift
-      tableau_taches = tableau_taches.reject{|tache| tache[:id] == id.to_i}
-else
-    puts "Commande non reconnue"
+    tache
 end
 
-puts "*****TASKMAN*****"
-puts "LISTE DES TACHES"
+#Récupération du premier argument et mise en minuscule
+command = ARGV.shift.downcase
 
-tableau_taches.each do |tache|
-    puts "#{tache[:id]} - #{tache[:content]} (#{tache[:flags].join(" ")})"
-end
+parser_command(command)
+afficher
