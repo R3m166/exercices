23e33d4d334778fee8e72ea2054c7c3605591ab9
R3m1 <r3m1.informatique@gmail.com>
2018-08-01 19:37:54 +0200

    Code apres chap. 9 : les modules

--- taskman/taskman.rb
+++ taskman/taskman.rb
@@ -6,104 +6,109 @@
 #Utilisation de p plutôt que puts pour conserver la forme de tableau à l'affichage
 #p ARGV
 
-#Initialisation du tableau des taches
-$tableau_taches = [
-    {id: 0, :content => "Ameliorer taskman", flags: %w(important urgent)}
-    #[0, "Ameliorer taskman", "flags", "important urgent"]
-]
-
-OPTIONS_DEFAULT = {
-    flags: [],
-    date: nil
-}
-
-def afficher
-    puts "*****TASKMAN*****"
-    puts "LISTE DES TACHES"
-
-    $tableau_taches.each do |tache|
-        puts "#{tache[:id]} - #{tache[:content]} (#{tache[:flags].join(" ")})"
-    end
-end
-
-def supprimer id
-    $tableau_taches = $tableau_taches.reject{|tache| tache[:id] == id.to_i}
-end
-
-def ajouter params
-    commande = params.shift
-
-    tache = {}
-
-    tache[:id] = $tableau_taches.map{ |tache| tache[:id] }.max + 1
-
-    
-    tache.merge!(OPTIONS_DEFAULT)
-
-    tache[:content] = commande
-
-    params.each do |argument|
-        champ, valeur = argument.split(':')
-        if champ == "flags"
-            tache[champ.to_sym] = valeur.split(",")
-        else
-            tache[champ.to_sym] = valeur
-        end
-    end
-
-    tache
-end
-
-def afficher_aide
-    puts "taskman [commande] [contenu] [options....]"
-    puts "------------------------------------------"
-    $commandes.each do |k, v|
-        puts "#{v[:keyword]} #{v[:arguments]}\t * #{v[:description]}"
-    end
-end
-
-$commandes = {}
-
-def register_command command, arguments, description, &block
-    $commandes[command] = {
-        keyword: command,
-        arguments: arguments,
-        description: description,
-        block: block
+module Task
+    OPTIONS_DEFAULT = {
+        flags: [],
+        date: nil
     }
+
+    #Initialisation du tableau des taches
+    @tableau_taches = [
+        {id: 0, :content => "Ameliorer taskman", flags: %w(important urgent)}
+    ]
+
+    def self.supprimer id
+        @tableau_taches = @tableau_taches.reject{|tache| tache[:id] == id.to_i}
+        #ou @tableau_taches.reject!{|tache| tache[:id] == id.to_i}
+    end
+    
+    def self.ajouter params
+        commande = params.shift
+    
+        tache = {}
+    
+        tache[:id] = @tableau_taches.map{ |tache| tache[:id] }.max + 1
+    
+        
+        tache.merge!(Task::OPTIONS_DEFAULT)
+    
+        tache[:content] = commande
+    
+        params.each do |argument|
+            champ, valeur = argument.split(':')
+            if champ == "flags"
+                tache[champ.to_sym] = valeur.split(",")
+            else
+                tache[champ.to_sym] = valeur
+            end
+        end
+    
+        @tableau_taches << tache
+    end
+
+    def self.afficher
+        puts "*****TASKMAN*****"
+        puts "LISTE DES TACHES"
+    
+        @tableau_taches.each do |tache|
+            puts "#{tache[:id]} - #{tache[:content]} (#{tache[:flags].join(" ")})"
+        end
+    end
 end
 
-def lancer_commande!
-    $command = ARGV.shift
-    commande_execute = false
-    $commandes.each do |k, v|
-        if k == $command
-            commande_execute = true
-            v[:block].call(ARGV)
+
+module Commande
+    @commandes = {}
+
+    def self.register command, arguments, description, &block
+        @commandes[command] = {
+            keyword: command,
+            arguments: arguments,
+            description: description,
+            block: block
+        }
+    end
+
+    def self.afficher_aide
+        puts "taskman [commande] [contenu] [options....]"
+        puts "------------------------------------------"
+        @commandes.each do |k, v|
+            puts "#{v[:keyword]} #{v[:arguments]}\t * #{v[:description]}"
         end
     end
 
-    if not commande_execute #or unless
-        afficher_aide
+    def self.lancer!
+        command = ARGV.shift
+        commande_execute = false
+        @commandes.each do |k, v|
+            if k == command
+                commande_execute = true
+                v[:block].call(ARGV)
+            end
+        end
+    
+        if not commande_execute #or unless
+            afficher_aide
+        end
     end
 end
 
-register_command 'add', ':contenu (options...)', 'Crée une nouvelle tâche.' do |arguments|
-    $tableau_taches << ajouter(arguments)
+Commande.register 'add', ':contenu (options...)', 'Crée une nouvelle tâche.' do |arguments|
+    Task.ajouter arguments
 end
-register_command 'del', ':id', 'Supprimer une tache.' do |arguments|
-    supprimer arguments.shift.to_i
+Commande.register 'del', ':id', 'Supprimer une tache.' do |arguments|
+    Task.supprimer arguments.shift.to_i
 end
-register_command 'mod', ':id (options...)', 'Modifie une tache.' do |arguments|
+Commande.register 'mod', ':id (options...)', 'Modifie une tache.' do |arguments|
     puts "Commande MOD"
 end
-register_command 'list', ':filtre', 'Liste les taches.' do |arguments|
-    afficher
+Commande.register 'list', ':filtre', 'Liste les taches.' do |arguments|
+    Task.afficher
 end
-register_command 'clear', '', 'Supprimer toute les taches.' do |arguments|
+Commande.register 'clear', '', 'Supprimer toute les taches.' do |arguments|
     #A FAIRE : Supprimer toute les taches
 end
 
-lancer_commande!
+Commande.lancer!
 
-afficher
+Task.afficher
