06303da9c93cdb89cee9214dcb1c738ee97caac5
R3m1 <r3m1.informatique@gmail.com>
2018-08-04 13:38:07 +0200

    Code apres chap. 10 : les classes 2/2 (heritage)

--- taskman/taskman.rb
+++ taskman/taskman.rb
@@ -50,6 +50,10 @@ class Task
     
         @tableau_taches << new_task
     end
+
+    def self.get_task id
+        @tableau_taches.select{ |tache| tache.id == id }.first
+    end
     
     def self.supprimer id
         @tableau_taches = @tableau_taches.reject{|tache| tache.id == id.to_i}
@@ -70,30 +74,25 @@ end
 module Commande
     @commandes = {}
 
-    def self.register command, arguments, description, &block
-        @commandes[command] = {
-            keyword: command,
-            arguments: arguments,
-            description: description,
-            block: block
-        }
+    def self.register action
+        @commandes[action.command] = action
     end
 
     def self.afficher_aide
         puts "taskman [commande] [contenu] [options....]"
         puts "------------------------------------------"
-        @commandes.each do |k, v|
-            puts "#{v[:keyword]} #{v[:arguments]}\t * #{v[:description]}"
+        @commandes.each do |k, action|
+            puts action.to_s
         end
     end
 
     def self.lancer!
         command = ARGV.shift
         commande_execute = false
-        @commandes.each do |k, v|
+        @commandes.each do |k, action|
             if k == command
                 commande_execute = true
-                v[:block].call(ARGV)
+                action.apply(ARGV)
             end
         end
     
@@ -101,23 +100,72 @@ module Commande
             afficher_aide
         end
     end
+
+    class Action
+        attr_accessor :command, :arguments, :description, :block
+
+        def initialize command, arguments, description, &block
+            @command = command
+            @arguments = arguments
+            @description = description
+            @block = block
+        end
+
+        def apply arguments
+            block.call(arguments)
+        end
+
+        def register!
+            Commande.register(self)
+        end
+
+        def to_s
+            puts "#{@command} #{@arguments}\t * #{@description}"
+        end
+    end
+
+    class TaskAction < Action
+        def initialize command, arguments, description, &block
+            super command, arguments, description, &block
+        end 
+
+        def apply arguments
+            id = arguments.shift.to_i
+            task = Task.get_task id
+
+            if task.nil?
+                puts "La tache #{id} n'existe pas!"
+                exit
+            end
+
+            block.call(task, arguments)
+        end
+
+        def to_s
+            puts "#{@command} :id #{@arguments}\t * #{@description}"
+        end
+    end
 end
 
-Commande.register 'add', ':contenu (options...)', 'Crée une nouvelle tâche.' do |arguments|
-    Task.ajouter arguments
-end
-Commande.register 'del', ':id', 'Supprimer une tache.' do |arguments|
-    Task.supprimer arguments.shift.to_i
-end
-Commande.register 'mod', ':id (options...)', 'Modifie une tache.' do |arguments|
-    puts "Commande MOD"
-end
-Commande.register 'list', ':filtre', 'Liste les taches.' do |arguments|
-    Task.afficher
-end
-Commande.register 'clear', '', 'Supprimer toute les taches.' do |arguments|
-    #A FAIRE : Supprimer toute les taches
-end
+
+Commande::Action.new('add', ':contenu (options...)', 'Crée une nouvelle tâche.') do |arguments|
+    Task.ajouter(arguments)
+end.register!
+
+Commande::TaskAction.new('del', '', 'Supprimer une tache.') do |task, args|
+    Task.supprimer task.id
+end.register!
+
+
+#Commande.register 'mod', ':id (options...)', 'Modifie une tache.' do |arguments|
+#    puts "Commande MOD"
+#end
+#Commande.register 'list', ':filtre', 'Liste les taches.' do |arguments|
+#    Task.afficher
+#end
+#Commande.register 'clear', '', 'Supprimer toute les taches.' do |arguments|
+#    #A FAIRE : Supprimer toute les taches
+#end
 
 Commande.lancer!
 
